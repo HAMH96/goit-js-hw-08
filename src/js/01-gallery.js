19b8c4e64e886a338ce052b65503d26a2df74fef
HAMH96 <hmoraleshoyos@gmail.com>
2023-05-06 01:41:31 -0500

    Listo para primera entrega

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,27 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const itemsGallery = galleryItems
+  .map(
+    galleryItem =>
+      `<a class="gallery__link" href=${galleryItem.original}>
+        <img class="gallery__image"
+        src=${galleryItem.preview} 
+        alt=${galleryItem.description}/>
+        </a>`
+  )
+  .join('');
 
-console.log("hola");
+gallery.insertAdjacentHTML('afterbegin', itemsGallery);
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captions: true,
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+});
+lightbox.on('show.simplelightbox');
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,59 @@
+import throttle from 'lodash.throttle';
 
+const feedbackForm = document.querySelector('.feedback-form');
+const feedbackFormChildrens = feedbackForm.children;
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+// Adicion de selectores de clase a los elementos del formulario
+for (let element of feedbackFormChildrens) {
+  if (element.firstElementChild != null) {
+    element.firstElementChild.classList.add(
+      `feedback-form__${element.firstElementChild.getAttribute('name')}`
+    );
+  } else {
+    element.classList.add(`feedback-form__${element.getAttribute('type')}`);
+  }
+}
+
+// Obtencion de las referencias de clase de los selectores del formulario
+const [emailInput, messageInput, submitButton] = feedbackForm.querySelectorAll(
+  '.feedback-form__email, .feedback-form__message, .feedback-form__submit'
+);
+
+//Objeto de inicio de valores
+let formValues = {
+  email: 'fakemail@fake.com',
+  message: 'new001',
+};
+
+// Fijacion de los valores guardados en localStorage en el formulario
+const formValueSaved = localStorage.getItem('feedback-form-state');
+const feedback = JSON.parse(formValueSaved);
+if (formValueSaved != null) {
+  emailInput.value = feedback.email;
+  messageInput.value = feedback.message;
+}
+
+// Funcion callback que guarda los valores en localStorage
+const setFormValues = () => {
+  formValues = { email: emailInput.value, message: messageInput.value };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formValues));
+};
+
+// Funcion que borra los valores del localStorage y muestra en consola
+const checkFormValues = event => {
+  if (emailRegex.test(emailInput.value)) {
+    event.preventDefault();
+    console.log(formValues);
+    emailInput.value = '';
+    messageInput.value = '';
+    localStorage.removeItem('feedback-form-state');
+  } else {
+    alert('El correo no tiene el formato adecuado');
+  }
+};
+
+//Evento input que dispara la funcion callback cada que se teclea
+feedbackForm.addEventListener('input', throttle(setFormValues, 500));
+//Evento que dispara la funcion callback al hacer click
+submitButton.addEventListener('click', checkFormValues);
